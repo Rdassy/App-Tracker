e143183ed19afeccc1ce70888df7c91c76194e87
Jonathan Acra <jonathanacra@gmail.com>
2022-06-03 21:28:29 +0000

    updated user table

--- src/api/models.py
+++ src/api/models.py
@@ -5,6 +5,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(255), nullable = False)
+    last_name = db.Column(db.String(255), nullable = False)
     password = db.Column(db.String(80), unique=False, nullable=False)
 
     def __repr__(self):
@@ -14,6 +16,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name":self.last_name,
         }
 
 class Links(db.Model):
