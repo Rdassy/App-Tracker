879231e936b5186b6609ead1f44ceab17175c099
Jonathan Acra <jonathanacra@gmail.com>
2022-06-03 20:40:14 +0000

    added links table to backend

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +14,19 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Links(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    link_type = db.Column(db.String(255))
+    link = db.Column(db.String(255), nullable = True)
+    user = db.relationship("User")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "link_type":self.link_type,
+            "link":self.link
         }
