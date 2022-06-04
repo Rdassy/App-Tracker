0c21edc2ffd783f1e6d97cfa12b4cf7f0db16562
Jonathan Acra <jonathanacra@gmail.com>
2022-06-04 20:25:00 +0000

    working login and registration, edit on application modal popup

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Application, Links, Notes, Interaction
+from api.models import db, User, Application, Links, Note, Interaction
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -15,6 +15,8 @@ export const Login = () => {
   //declare states here vvvv
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   return (
     <div className="dashBody w-100">
@@ -107,6 +109,11 @@ export const Login = () => {
                         <button
                           class="btn btn-outline-light btn-lg px-5"
                           type="submit"
+                          onClick={() => {
+                            actions
+                              .createNewSession(email, password)
+                              .then(history.push("/home"));
+                          }}
                         >
                           Login
                         </button>
@@ -131,7 +138,111 @@ export const Login = () => {
               role="tabpanel"
               aria-labelledby="pills-register-tab"
             >
-              Register
+              <div class="me-0 d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                  <div class="loginCard border-0 card text-white">
+                    <div class="card-body text-center">
+                      <div class="mb-md-5 mt-md-4 pb-5">
+                        <div class="form-outline form-white mb-4">
+                          <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Example@example.com"
+                            id="typeEmailX"
+                            class="form-control form-control-lg pt-3"
+                          />
+                          <label className="form-label mt-2" for="typeEmailX">
+                            Email
+                          </label>
+                        </div>
+                        <div class="form-outline form-white mb-4">
+                          <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Enter Password (Case Sensitive)"
+                            id="typePasswordX"
+                            class="form-control form-control-lg pt-3"
+                          />
+                          <label
+                            className="form-label mt-2"
+                            for="typePasswordX"
+                          >
+                            Password
+                          </label>
+                        </div>
+                        <div class="form-outline form-white mb-4">
+                          <input
+                            type="text"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            placeholder="First Name"
+                            id="typeFirstNameX"
+                            class="form-control form-control-lg pt-3"
+                          />
+                          <label
+                            className="form-label mt-2"
+                            for="typeFirstNameX"
+                          >
+                            First Name
+                          </label>
+                        </div>
+                        <div class="form-outline form-white mb-4">
+                          <input
+                            type="text"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                            placeholder="Last Name"
+                            id="typeLastNameX"
+                            class="form-control form-control-lg pt-3"
+                          />
+                          <label
+                            className="form-label mt-2"
+                            for="typeLastNameX"
+                          >
+                            Last Name
+                          </label>
+                        </div>
+                        <p class="small mb-5 pb-lg-2">
+                          <a class="text-white-50" href="#!">
+                            Forgot password?
+                          </a>
+                        </p>
+                        <button
+                          class="btn btn-outline-light btn-lg px-5"
+                          type="submit"
+                          onClick={async () => {
+                            try {
+                              const payload1 = await actions.createUser(
+                                email,
+                                password,
+                                firstName,
+                                lastName
+                              );
+                              const payload2 = actions.createNewSession(
+                                email,
+                                password
+                              );
+                              history.push("/home");
+                            } catch {}
+                          }}
+                        >
+                          Login
+                        </button>
+                      </div>
+                      <div>
+                        <p class="mb-0">
+                          Don't have an account?{" "}
+                          <a href="#!" class="text-white-50 fw-bold">
+                            Sign Up
+                          </a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
